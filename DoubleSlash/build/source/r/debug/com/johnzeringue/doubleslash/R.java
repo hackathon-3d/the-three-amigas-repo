b17239fe7e325c1665a3b21d74e11e30e6e1fecb
zeringus <john.joseph.zeringue@gmail.com>
2013-08-24 12:27:51 -0400

    Some UI tinkering.

--- DoubleSlash/build/source/r/debug/com/johnzeringue/doubleslash/R.java
+++ DoubleSlash/build/source/r/debug/com/johnzeringue/doubleslash/R.java
@@ -26,27 +26,30 @@ public final class R {
         public static final int folder=0x7f020002;
         public static final int folder_states=0x7f020003;
         public static final int folderpressed=0x7f020004;
-        public static final int header=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int icon=0x7f020007;
-        public static final int logo=0x7f020008;
-        public static final int newfile_states=0x7f020009;
-        public static final int newfolder=0x7f02000a;
-        public static final int newfolder_states=0x7f02000b;
-        public static final int newfolderpressed=0x7f02000c;
-        public static final int newpage=0x7f02000d;
-        public static final int newpage_presssed=0x7f02000e;
-        public static final int page=0x7f02000f;
-        public static final int page_pressed=0x7f020010;
+        public static final int grid=0x7f020005;
+        public static final int header=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int icon=0x7f020008;
+        public static final int logo=0x7f020009;
+        public static final int newfile_states=0x7f02000a;
+        public static final int newfolder=0x7f02000b;
+        public static final int newfolder_states=0x7f02000c;
+        public static final int newfolderpressed=0x7f02000d;
+        public static final int newpage=0x7f02000e;
+        public static final int newpage_presssed=0x7f02000f;
+        public static final int page=0x7f020010;
+        public static final int page_pressed=0x7f020011;
+        public static final int slash=0x7f020012;
+        public static final int trash=0x7f020013;
     }
     public static final class id {
         public static final int action_settings=0x7f080006;
-        public static final int button=0x7f080004;
+        public static final int button=0x7f080002;
         public static final int editText=0x7f080000;
         public static final int editText2=0x7f080001;
         public static final int gridView=0x7f080005;
-        public static final int note_body=0x7f080003;
-        public static final int note_name=0x7f080002;
+        public static final int note_body=0x7f080004;
+        public static final int note_name=0x7f080003;
     }
     public static final class layout {
         public static final int activity_edit_note=0x7f030000;
--- DoubleSlash/src/main/java/com/johnzeringue/doubleslash/EditNoteActivity.java
+++ DoubleSlash/src/main/java/com/johnzeringue/doubleslash/EditNoteActivity.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 public class EditNoteActivity extends Activity {
     private NotesDBAdapter _adapter;
     private long _id;
+    private boolean _deleting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        _deleting = false;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
 
@@ -27,15 +31,26 @@ public class EditNoteActivity extends Activity {
         ((TextView) findViewById(R.id.editText2)).setText(cursor.getString(cursor.getColumnIndex("body")));
     }
 
+    public void deleteNote(View view) {
+        _adapter.deleteNote(_id);
+        _adapter.close();
+
+        _deleting = true;
+
+        finish();
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
 
-        _adapter.updateNote(_id,
-                String.valueOf(((TextView) findViewById(R.id.editText)).getText()),
-                String.valueOf(((TextView) findViewById(R.id.editText2)).getText()));
+        if (!_deleting) {
+            _adapter.updateNote(_id,
+                    String.valueOf(((TextView) findViewById(R.id.editText)).getText()),
+                    String.valueOf(((TextView) findViewById(R.id.editText2)).getText()));
 
-        _adapter.close();
+            _adapter.close();
+        }
     }
 
 
